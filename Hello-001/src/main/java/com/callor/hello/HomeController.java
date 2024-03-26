a98982acea523fb20af92cf3377a97ff3e382210
WHM0304 <whm0304@naver.com>
2024-03-26 12:54:42 +0900

    hello

---
+++ Hello-001/src/main/java/com/callor/hello/HomeController.java
@@ -0,0 +1,21 @@
+package com.callor.hello;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class HomeController { //컨트롤러라고 붙어있는것들은 라우터!!
+	
+	
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home"; // home.jsp 를 리턴(렌더링) 해라
+	}
+	
+}
