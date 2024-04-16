6d15ff11b2d1b91e481dff7b68add1918c63722f
WHM0304 <whm0304@naver.com>
2024-04-16 12:52:49 +0900

    0416

--- SpMVC-user-03/src/main/java/com/callor/hello/controller/UserController.java
+++ SpMVC-user-03/src/main/java/com/callor/hello/controller/UserController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.callor.hello.models.UserVO;
+import com.callor.hello.service.UserService;
+
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
+	
+	private final UserService userService;
+	
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
 	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String join() {
@@ -16,8 +25,10 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(String u_name , String u_password) {
-		log.debug("회원가입정보 {} , {}" , u_name, u_password);
+	public String join(UserVO userVO) {
+		log.debug("회원가입정보 {}  ,{}" ,userVO.getUsername(),userVO.getPassword() );
+		
+		UserVO ret = userService.createUser(userVO);
 		return null;
 	}
 	
