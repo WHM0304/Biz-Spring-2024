86fbe29a5894bfb2d1ee60fab852a1fd205d9405
WHM0304 <whm0304@naver.com>
2024-04-16 15:42:53 +0900

    0416

--- SpMVC-user-03/document/database(2024-04-16).sql
+++ SpMVC-user-03/document/database(2024-04-16).sql
@@ -12,3 +12,10 @@ SELECT * FROM tbl_roles;
 SELECT * FROM tbl_users U
 	JOIN tbl_roles R
 		ON U.username = R.r_username;
+
+
+DELETE FROM tbl_users WHERE username = 'callor';
+DELETE FROM tbl_users WHERE username = 'callor88';
+
+
+SELECT LENGTH(password) FROM tbl_users;
