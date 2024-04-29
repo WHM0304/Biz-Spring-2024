f72407b85876b3657e0a6eea23a3cb03f9154c4b
WHM0304 <whm0304@naver.com>
2024-04-29 11:50:42 +0900

    0429

--- SpMVC-gallery-02/src/main/java/com/callor/gallery/dao/GalleryDao.java
+++ SpMVC-gallery-02/src/main/java/com/callor/gallery/dao/GalleryDao.java
@@ -7,5 +7,7 @@ public interface GalleryDao extends GenericDao<GalleryVO, String>{
 	
 	public void create_table(String dumy);
 	
+	public GalleryVO findById(String pk);
+	
 
 }
--- SpMVC-gallery-02/src/main/java/com/callor/gallery/service/impl/GalleryServiceImpl.java
+++ SpMVC-gallery-02/src/main/java/com/callor/gallery/service/impl/GalleryServiceImpl.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,10 +15,14 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import com.callor.gallery.dao.GalleryDao;
 import com.callor.gallery.dao.ImageDao;
 import com.callor.gallery.models.GalleryVO;
+import com.callor.gallery.models.ImageVO;
 import com.callor.gallery.service.FileUploadService;
 import com.callor.gallery.service.GalleryService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class GalleryServiceImpl implements GalleryService {
 
 	private final FileUploadService fileUploadService;
@@ -48,27 +54,22 @@ public class GalleryServiceImpl implements GalleryService {
 
 	}
 
+	
+	/*
+	 * Single 파일을 업로드 했을때 사용하는 method
+	 */
 	@Override
 	public GalleryVO createGallery(GalleryVO galleryVO, MultipartFile image_file) throws Exception {
 		
-		galleryVO.setG_id(UUID.randomUUID().toString());
+		
 		galleryVO.setG_origin_image(image_file.getOriginalFilename());
 		
 		// 파일을 업로드하고 return 받은 변형된 이름을 VO 에 setting
 		String fileName = fileUploadService.fileUpload(image_file);
 		galleryVO.setG_up_image(fileName);
 		
-		
-		LocalDateTime lt = LocalDateTime.now();
-		DateTimeFormatter date = DateTimeFormatter.ofPattern("YYYY-MM-dd");
-		DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm:ss");
-		
-		galleryVO.setG_date(lt.format(date));
-		galleryVO.setG_time(lt.format(time));
-		galleryVO.setG_author("whm0304@naver.com");
-		
-		
-		
+		// id,날짜, 시간, setting 대신해주기
+		setGalleryOptions(galleryVO);
 		
 		
 		int ret = galleryDao.insert(galleryVO);
@@ -79,12 +80,51 @@ public class GalleryServiceImpl implements GalleryService {
 
 	}
 
+	
+	/*
+	 * MultiFile 을 업로드 했을때 사용할 method
+	 */
+	@Transactional
 	@Override
 	public List<GalleryVO> createGallery
 	(GalleryVO galleryVO, MultipartHttpServletRequest image_files)
 			throws Exception {
-		List<String> result = fileUploadService.filesUpload(image_files);
+
+		
+		setGalleryOptions(galleryVO);
+		// 기본 이미지 정보가 없으므로 공백으로 세팅
+		galleryVO.setG_origin_image("");
+		galleryVO.setG_up_image("");
+//		String i_gid = galleryVO.getG_id();
+		
+		
+		
+		int gRet = galleryDao.insert(galleryVO);
+		// mapper 의 selectKey 에서 설정한 g_id 값을 참조할수 있다.
+		String i_gid = galleryVO.getG_id();
+		log.debug("확인 -----{}",i_gid);
+		
+		List<ImageVO> resultNames = fileUploadService.filesUpload(image_files);
+		
+		
+		int iret = imageDao.inserts(i_gid,resultNames);
+		
+		
+		
+		
+		
 		return null;
+	}
+	
+	
+	private void setGalleryOptions(GalleryVO vo) {
+		LocalDateTime lt = LocalDateTime.now();
+		DateTimeFormatter date = DateTimeFormatter.ofPattern("YYYY-MM-dd");
+		DateTimeFormatter time = DateTimeFormatter.ofPattern("HH:mm:ss");
+		vo.setG_id(UUID.randomUUID().toString());
+		vo.setG_date(lt.format(date));
+		vo.setG_time(lt.format(time));
+		vo.setG_author("whm0304@naver.com");
 	}
 
 }
