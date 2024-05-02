f8222fc200622c72a24c8091b6f0bd3811ea8a5d
WHM0304 <whm0304@naver.com>
2024-05-02 11:50:25 +0900

    0502

--- SpMVC-gallery-02/document/database(2024-04-30).sql
+++ SpMVC-gallery-02/document/database(2024-04-30).sql
@@ -7,9 +7,22 @@ email	VARCHAR(125)	NOT NULL	,
 tel	VARCHAR(125)	NOT NULL	
 );
 
-CREATE TABLE tbl_roles(
-r_username	VARCHAR(125)		PRIMARY KEY,
-r_role	VARCHAR(125)	NOT NULL	
+CREATE TABLE IF NOT EXISTS tbl_roles(
+	r_username	VARCHAR(125) ,
+	r_role	VARCHAR(125)	NOT NULL,	
+    
+    CONSTRAINT PK_ROLE
+    PRIMARY KEY(r_username,r_role),
+    
+	CONSTRAINT FK_USER
+	FOREIGN KEY (r_username)
+    REFERENCES tbl_users(username) ON DELETE CASCADE
+    
 );
+DESC tbl_roles;
 DROP TABLE tbl_roles;
 DROP TABLE tbl_users;
+SELECT * FROM tbl_users;
+SELECT * FROM tbl_roles;
+
+DESC tbl_users;
--- SpMVC-gallery-02/src/main/java/com/callor/gallery/service/auth/AuthorProviderImpl.java
+++ SpMVC-gallery-02/src/main/java/com/callor/gallery/service/auth/AuthorProviderImpl.java
@@ -11,30 +11,68 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.callor.gallery.dao.RoleDao;
+import com.callor.gallery.dao.UserDao;
+import com.callor.gallery.models.RoleVO;
+import com.callor.gallery.models.UserVO;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service("authorProviderImpl")
 public class AuthorProviderImpl implements AuthenticationProvider{
+	private final UserDao userDao;
+	private final RoleDao roleDao;
+	private final PasswordEncoder passwordEncoder;
+
+
+
+	public AuthorProviderImpl(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
+		super();
+		this.userDao = userDao;
+		this.roleDao = roleDao;
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		
 		String username = authentication.getName();
 		String password = authentication.getCredentials().toString();
+		log.debug("{}",username);
 		
-		if(username == null || username.isBlank() || !username.equalsIgnoreCase("callor")) {
+		if(username == null || username.isBlank()) {
 			throw new UsernameNotFoundException("사용자이름 확인");
 		}
-		if(password == null || password.isBlank() || !password.equals("1234")) {
+		
+		UserVO userVO = userDao.findById(username);
+		if(userVO == null || !userVO.getUsername().equals(username)) {
+			throw new UsernameNotFoundException("없는 사용자이름 입니다.");
+		}
+		if(password == null || password.isBlank())
+				 {
 			throw new BadCredentialsException("비밀번호 확인");
 		}
+		if(!passwordEncoder.matches(password,userVO.getPassword()))
+				 {
+			throw new BadCredentialsException("비밀번호가 일치하지 않음");
+		}
 		List<GrantedAuthority> grantList = new ArrayList<>();
-		grantList.add(new SimpleGrantedAuthority("USER"));
-		grantList.add(new SimpleGrantedAuthority("ADMIN"));
+		List<RoleVO> roles = roleDao.findByUsername(username);
+		for(RoleVO vo : roles) {
+			grantList.add(new SimpleGrantedAuthority(vo.getR_role()));
+		}
+		
+		
+		
+//		grantList.add(new SimpleGrantedAuthority("USER"));
+//		grantList.add(new SimpleGrantedAuthority("ADMIN"));
 		
 		UsernamePasswordAuthenticationToken token
-		= new UsernamePasswordAuthenticationToken(username, password,grantList);
+		= new UsernamePasswordAuthenticationToken(userVO, password,grantList);
 		
 		
 		
