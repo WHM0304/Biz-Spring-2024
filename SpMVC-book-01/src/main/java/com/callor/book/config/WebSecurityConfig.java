ff04250e6b8617c448782510e3e549270a596edb
WHM0304 <whm0304@naver.com>
2024-05-21 11:50:37 +0900

    0521

---
+++ SpMVC-book-01/src/main/java/com/callor/book/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.callor.book.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/*
+ * 프로젝트에 Security Filter를 적용하기 위한 클래스
+ * Spring Security 가 프로젝트에서 활성화 된다.
+ */
+
+public class WebSecurityConfig extends AbstractSecurityWebApplicationInitializer{
+	
+
+}
